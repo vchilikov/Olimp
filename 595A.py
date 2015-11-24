7f7188622cbb2364454d8bc6a7c9d9391ea34850
vchilikov@gmail.com <tel73700>
2015-11-24 23:49:14 +0300

    Решение задачи 595A, 599A

--- 595A.py
+++ 595A.py
@@ -2,6 +2,5 @@ n, m = map(int, input().split())
 res = 0
 for _ in range(n):
     s = input().split()
-    for j in range(m):
-        res += s[j * 2] == '1' or s[j * 2 + 1] == '1'
+    res += sum(map(lambda x, y: int(x) or int(y), s[::2], s[1::2]))
 print(res)
