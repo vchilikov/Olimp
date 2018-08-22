0b74c7a82ad86541ac686df48af4ff6cb2f0c02d
v.chilikov <v.chilikov@rambler-co.ru>
2018-08-22 15:30:47 +0300

    решение 1027D.py

--- 1027D.py
+++ 1027D.py
@@ -1,65 +1,52 @@
-import os
 from datetime import datetime
-import resource
-
-import psutil as psutil
+from random import randint
 
 start = datetime.now()
 
-# n = int(input())
-# c = list(map(int, input().split()))
-# a = list(map(lambda x: int(x) - 1, input().split()))
-n = 5000
-c = [1] * n
-a = [i + 1 for i in range(n)]
-a[n - 1] = 0
+n = int(input())
+c = list(map(int, input().split()))
+a = list(map(lambda x: int(x) - 1, input().split()))
+# n = 1000
+# c = [1] * n
+# a = [randint(0, n-1) for i in range(n)]
 
-position = [set() for i in range(n)]
+b = [None] * n
+processed = [False for i in range(n)]
 for i in range(n):
+    if not b[a[i]]:
+        b[a[i]] = {i}
+    else:
+        b[a[i]].add(i)
+
+rings = []
+
+
+def processed_ring(ring):
+    copy_ring = ring.copy()
+    while copy_ring:
+        new_ring = set()
+        for el in copy_ring:
+            processed[el] = True
+            if b[el]:
+                new_ring |= b[el]
+        copy_ring = new_ring - copy_ring
+
+
+for i in range(n):
+    if processed[i]:
+        continue
+    ring = set()
+    position = set()
     next_pos = i
-    while next_pos not in position[i]:
-        position[i].add(next_pos)
-
+    while next_pos not in position:
+        position.add(next_pos)
         next_pos = a[next_pos]
+    while next_pos not in ring:
+        ring.add(next_pos)
+        next_pos = a[next_pos]
+    processed_ring(ring)
+    rings.append(ring)
 
-for i in range(n - 1):
-    if not position[i]:
-        continue
-    for j in range(i + 1, n):
-        if not position[j]:
-            continue
-        intersection = position[i] & position[j]
-        if intersection:
-            position[i] = intersection
-            position[j] = None
-
-process = psutil.Process(os.getpid())
-print(process.memory_info().rss)
-
-res = 0
-for el in position:
-    if not el:
-        continue
-    res += min(c[i] for i in el)
-
+res = sum(min(c[i] for i in ring) for ring in rings)
 print(res)
 print(datetime.now() - start)
-
-
-
-usage = resource.getrusage(resource.RUSAGE_SELF)
-
-for name, desc in [
-    ('ru_utime', 'User time'),
-    ('ru_stime', 'System time'),
-    ('ru_maxrss', 'Max. Resident Set Size'),
-    ('ru_ixrss', 'Shared Memory Size'),
-    ('ru_idrss', 'Unshared Memory Size'),
-    ('ru_isrss', 'Stack Size'),
-    ('ru_inblock', 'Block inputs'),
-    ('ru_oublock', 'Block outputs'),
-    ]:
-    print('%-25s (%-10s) = %s' % (desc, name, getattr(usage, name)))
-
-process = psutil.Process(os.getpid())
-print(process.memory_info().rss)
