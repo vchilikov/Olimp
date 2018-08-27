0c9a4407934ba70c5dc7b6cba901b4e948c1647d
v.chilikov <v.chilikov@rambler-co.ru>
2018-08-27 23:37:11 +0300

    Решение 580C.py

--- 580C.py
+++ 580C.py
@@ -1,23 +1,31 @@
-from collections import  defaultdict
+from collections import defaultdict
 
 n, m = map(int, input().split())
 a = list(map(int, input().split()))
-d = defaultdict(list)
+b = [0] * n
+d = defaultdict(set)
 
 for i in range(n - 1):
-    x, y = tuple(map(lambda x: int(x) - 1, input().split()))
-    d[x].append(y)
+    x, y = map(lambda x: int(x) - 1, input().split())
+    d[x].add(y)
+    d[y].add(x)
 
-result = []
-
+cnt = 0
 s = {0}
-
+b[0] = a[0]
+processed = set()
 while s:
-    print(s)
+    processed.update(s)
     new_s = set()
     for el in s:
-        new_s.update(d[el])
+        children = d[el] - processed
+        if children:
+            for next_el in children:
+                b[next_el] = b[el] + 1 if a[next_el] else 0
+                if b[next_el] <= m:
+                    new_s.add(next_el)
+        elif b[el] <= m:
+            cnt += 1
+
     s = new_s
-
-
-
+print(cnt)
